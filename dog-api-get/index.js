8ef4620c10de8daeea416c44401b9f3bb0383c9e
aldibatyr@gmail.com <47831819+aldibatyr@users.noreply.github.com>
2019-05-11 11:28:46 -0700

    completed drills 1-2

---
+++ dog-api-get/index.js
@@ -0,0 +1,31 @@
+'use strict';
+
+function getRandomDogs(n) {
+  fetch('https://dog.ceo/api/breeds/image/random/'+n)
+    .then(response => response.json())
+    // .then make images
+    .then(responseJson => 
+      displayResults(responseJson.message))
+    
+    .catch(error => alert('Something went wrong. Try again later.'));
+}
+
+function displayResults(array) {
+  console.log(array);
+  for(let i=0; i<array.length; i++){
+    $('.js-dog-list').append(`<li> <img src='${array[i]}'>`);
+  }
+}
+
+function watchForm() {
+  $('form').submit(event => {
+    event.preventDefault();
+    let n = $('.js-get-number').val();
+    getRandomDogs(n);
+  });
+}
+
+$(function() {
+  console.log('App loaded! Waiting for submit!');
+  watchForm();
+});
